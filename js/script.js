319b904d5998105445039653e707ae437282bd93
Rina Caladis <119719820+RinaCLDS@users.noreply.github.com>
2024-04-14 22:52:23 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector('.main-video video');
 let title = document.querySelector('.main-video .title');
-
+  
 listVideo.forEach(video => {
   video.onclick = () => {
       listVideo.forEach(vid => vid.classList.remove('active'));
